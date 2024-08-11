8e3d1cddba39aa1da0ebece2710d62699b6f4a6b
Gihzx <145726882+Gihzx@users.noreply.github.com>
2024-08-10 23:15:11 -0300

    feat:navbar feito

--- lion/src/components/molecules/navBar/NavBar.jsx
+++ lion/src/components/molecules/navBar/NavBar.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import img from "../../../assets/LeonsEletronico 1.svg";
 import "../navBar/navBar.css";
 import { FiAlignJustify } from "react-icons/fi";
 import { FiShoppingCart } from "react-icons/fi";
 import { FiUser } from "react-icons/fi";
 function NavBar() {
+  const [input, setInput] = useState("Hinted search text");
   return (
     <>
       <div className="container">
@@ -12,7 +14,12 @@ function NavBar() {
           <h2>Lion Eletronics</h2>
         </div>
         <div className="menu">
-          <input type="text" placeholder="Hinted search text" />
+          <input
+            type="text"
+            // temos um input que pega o valor e exibi em outro local que vc escolher
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
           <div className="icones">
             {/* menino */}
             <FiUser color="#fff" size={28} />
